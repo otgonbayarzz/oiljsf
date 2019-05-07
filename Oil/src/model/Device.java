4479df093ca6bc4b8d335c0ddc8a60739f39c076
Otgonbayar <otgon92_eb@yahoo.com>
2019-05-07 17:43:41 +0800

    fixo

--- Oil/src/model/Device.java
+++ Oil/src/model/Device.java
@@ -1,11 +1,16 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.Serializable;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 
 public class Device implements Serializable {
 	/**
@@ -23,11 +28,26 @@ public class Device implements Serializable {
 
 	public void giveCommand(String command) {
 		this.lastResponse = "";
+		byte[] buff = new byte[10000];
+		buff[0] = 0x02;
 
 		try (Socket socket = new Socket(this.ip, this.port)) {
 
 			OutputStream output = socket.getOutputStream();
-			this.lastResponse = "Message";
+			DataOutputStream dos = new DataOutputStream(output);
+
+			byte[] b = command.getBytes(StandardCharsets.UTF_8);
+			for (int i = 0; i < b.length; i++) {
+				buff[i + 1] = b[i];
+			}
+			buff[b.length + 1] = 0x03;
+			dos.write(buff, 0, b.length + 2);
+			InputStream input = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+			String time = reader.readLine();
+
+			this.lastResponse = time;
 
 		} catch (UnknownHostException ex) {
 
